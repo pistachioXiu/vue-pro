1abd110efcd5d8e7de7af4cd1a1bd8365fce4eba
pistachio_xiu <811749882@qq.com>
2022-03-28 19:36:32 +0800

    first conmmit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+	</div>
 </template>
 
 <script>
@@ -10,9 +9,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+	name: 'HomeView',
+	components: {
+		HelloWorld,
+	},
 }
 </script>
